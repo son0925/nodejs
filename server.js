f604679bfe6bde9229b34cd06bdad0002e8e12b1
son0925 <shs00925@naver.com>
2024-04-20 17:47:18 +0900

    Router패턴변경

--- server.js
+++ server.js
@@ -4,26 +4,25 @@ const port = '4000';
 
 const app = express();
 
-const usersController = require('./controllers/users.controller');
-const postsController = require('./controllers/posts.controller');
+const usersRouter = require('./routes/users.router');
+const postsRouter = require('./routes/posts.router');
 
 app.use((req,res,next) => {
   const timer = Date.now();
   console.log(`${req.method} ${req.url}`);
   next();
   const diffTimer = Date.now();
-  console.log(`${req.method} ${req.url} ${diffTimer}ms`);
+  console.log(`${req.method} ${req.baseUrl} ${diffTimer}ms`);
 })
 app.use(express.json());
 
 app.get('/', (req,res) => {
   res.send('Hello Git & NodeJs');
 })
-app.get('/users', usersController.getUsers);
-app.get('/users/:userId', usersController.getUser);
-app.post('/users', usersController.postUser);
 
-app.get('/posts', postsController)
+app.use('/users', usersRouter);
+app.use('/posts', postsRouter);
+
 
 app.listen(port, (req,res) => {
   console.log(`Running on port ${port}`);
